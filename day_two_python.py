826f7d8f9e80582d97698ad295ea7ea795563020
Julian Anaya <juliananaya531@gmail.com>
2024-02-01 15:49:04 -0800

    day 2 learning data type

--- day_two_python.py
+++ day_two_python.py
@@ -12,3 +12,11 @@ print(123 + 345)
 # boolean
 True
 False
+
+age = input()
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+
+how_many_years_left = 90 - int(age)
+how_many_weeks = how_many_years_left * 52
+print("You have " + str(how_many_weeks) + " weeks left.")
