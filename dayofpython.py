b948a13a32076a894db4d3dcad82d09ffdfc5e98
Julian Anaya <juliananaya531@gmail.com>
2024-01-31 22:33:39 -0800

    learning printing basics of python day one

--- dayofpython.py
+++ dayofpython.py
@@ -1,1 +1,7 @@
-print("hello world")
+# print("hello world")
+
+# Write a program in main.py that prints the following notes on Python programming into the Output console. Your code should print all three lines.
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
