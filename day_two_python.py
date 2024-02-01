3c89a5368c605629afa8ec255afb49481c3a452f
Julian Anaya <juliananaya531@gmail.com>
2024-02-01 15:59:56 -0800

    day 2 final project of the day

--- day_two_python.py
+++ day_two_python.py
@@ -1,22 +1,33 @@
-# Data Types
+# # Data Types
 
-# string
-print("Hello"[1])
-print("123" + "345")
+# # string
+# print("Hello"[1])
+# print("123" + "345")
 
-# integer
-print(123 + 345)
+# # integer
+# print(123 + 345)
 
-# float
-3.1459
-# boolean
-True
-False
+# # float
+# 3.1459
+# # boolean
+# True
+# False
 
-age = input()
-# 🚨 Don't change the code above 👆
+# age = input()
+# # 🚨 Don't change the code above 👆
+# # Write your code below this line 👇
+
+# how_many_years_left = 90 - int(age)
+# how_many_weeks = how_many_years_left * 52
+# print("You have " + str(how_many_weeks) + " weeks left.")
+
+# If the bill was $150.00, split between 5 people, with 12% tip.
+
+# Each person should pay (150.00 / 5) * 1.12 = 33.6
+# Format the result to 2 decimal places = 33.60
+
+# Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+
 # Write your code below this line 👇
 
-how_many_years_left = 90 - int(age)
-how_many_weeks = how_many_years_left * 52
-print("You have " + str(how_many_weeks) + " weeks left.")
+print("Welcome to the tip calculator!")
