7f2ad27d1835d0693e5715480dccf11c967aebf0
Julian Anaya <juliananaya531@gmail.com>
2024-01-31 23:55:34 -0800

    final day1 project done

--- dayofpython.py
+++ dayofpython.py
@@ -9,9 +9,11 @@ print("print('what to print')")
 # 1. Create a greeting for your program.
 print("Welcome to making band name generator!")
 # 2. Ask the user for the city that they grew up in.
-
+print("What city did you grow up in?")
+city = input()
 # 3. Ask the user for the name of a pet.
-
+print("What is the name of your pet?")
+pet = input()
 # 4. Combine the name of their city and pet and show them their band name.
-
+print("Your band name should be " + city + " " + pet)
 # 5. Make sure the input cursor shows on a new line:
