b84fba4b50121428c25ce5abc5c7d7f6ca2d4c00
Julian Anaya <juliananaya531@gmail.com>
2024-02-03 13:53:02 -0800

    day 4 learning random numbers p2

--- dat_four_python.py
+++ dat_four_python.py
@@ -8,3 +8,15 @@ print(random_integer)
 # print(my_module.pi)
 random_float = random.random()
 print(random_float)
+
+names = names_string.split(", ")
+# The code above converts the input into an array seperating
+# each name in the input by a comma and space.
+# 🚨 Don't change the code above 👆
+# print(len(names))
+import random
+
+nuber_of_people = len(names) - 1
+random_int = random.randint(0, nuber_of_people)
+person_chosen = names[random_int]
+print(f"{person_chosen} is going to buy the meal today!")
