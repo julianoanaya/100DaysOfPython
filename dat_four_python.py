84c67b131439fe8437cab0436872aae8671e96d5
Julian Anaya <juliananaya531@gmail.com>
2024-02-03 14:35:23 -0800

    day 4 final lesson rock,paper,scissors game

--- dat_four_python.py
+++ dat_four_python.py
@@ -29,3 +29,42 @@ nuber_of_people = len(names) - 1
 random_int = random.randint(0, nuber_of_people)
 person_chosen = names[random_int]
 print(f"{person_chosen} is going to buy the meal today!")
+
+
+rock = """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+
+paper = """
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+"""
+
+scissors = """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+
+# Write your code below this line 👇
+
+import random
+
+# User input
+user_choice = int(
+    input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+)
+
+image = ["rock", "paper", "scissors"]
