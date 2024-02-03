04a482cf44573ab5b28bc9e78d2dc37917b417f7
Julian Anaya <juliananaya531@gmail.com>
2024-02-03 13:55:15 -0800

    day 4 learning random float numbers

--- dat_four_python.py
+++ dat_four_python.py
@@ -9,6 +9,15 @@ print(random_integer)
 random_float = random.random()
 print(random_float)
 
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+# # print(my_module.pi)
+# random_float = random.random() * 5
+# print(random_float)
+
+# love_score = random.randint(1, 100)
+# print(f"Your love score is {love_score}")
+
 names = names_string.split(", ")
 # The code above converts the input into an array seperating
 # each name in the input by a comma and space.
