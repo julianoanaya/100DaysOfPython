ef3eda6116aae623a6e4062e723516636e93e214
Julian Anaya <juliananaya531@gmail.com>
2024-02-03 14:58:32 -0800

    day 4 final lesson rock,paper,scissors game part 3

--- dat_four_python.py
+++ dat_four_python.py
@@ -69,6 +69,7 @@ user_choice = int(
     input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
 )
 print(image[user_choice])
+# if user_choice == 0:
 
 
 # computer choice
@@ -76,3 +77,14 @@ choices = ["rock", "paper", "scissors"]
 choice = len(choices) - 1
 computer_choice = random.randint(0, choice)
 print(image[computer_choice])
+
+if user_choice == computer_choice:
+    print("Draw")
+elif user_choice == 0 and computer_choice == 2:
+    print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+    print("You lose")
+elif computer_choice > user_choice:
+    print("You lose")
+elif user_choice > computer_choice:
+    print("You win!")
