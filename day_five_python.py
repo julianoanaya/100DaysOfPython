71c2730266f6ae4ff9a07eb44fbed9fae130bc15
Julian Anaya <juliananaya531@gmail.com>
2024-02-04 13:46:35 -0800

    day 5 learning loops w/range exercise 1

--- day_five_python.py
+++ day_five_python.py
@@ -52,3 +52,15 @@ total = 0
 for number in range(1, 101):
     total += number
 print(total)
+
+
+target = int(input())  # Enter a number between 0 and 1000
+# 🚨 Do not change the code above ☝️
+
+# Write your code here 👇
+
+
+total = 0
+for number in range(2, target + 1, 2):
+    total += number
+print(total)
