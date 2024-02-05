5ebd3fc55ca196bb98bedbdf0506b391a6726b52
Julian Anaya <juliananaya531@gmail.com>
2024-02-05 14:16:25 -0800

    day 6 learning def -built-in-functions reborgs world

--- day_six_python.py
+++ day_six_python.py
@@ -9,3 +9,41 @@ def my_function():
 
 
 my_function()
+
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+
+# def move_and_jump_reset():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+
+# move()
+# turn_left()
+# move()
+# turn_right()
+# move()
+# turn_right()
+# move()
+# turn_left()
+# move_and_jump_reset()
+# move_and_jump_reset()
+# move_and_jump_reset()
+# move_and_jump_reset()
+# move()
+# turn_left()
+# move()
+# turn_right()
+# move()
+# turn_right()
+# move()
