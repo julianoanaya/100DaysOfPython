e5aee556b74b418c2a7b3939a75885ab89851ec2
Julian Anaya <juliananaya531@gmail.com>
2024-02-07 15:01:44 -0800

    day 7 building hangman game todo 4 part 1 adding ending game

--- day_seven_python.py
+++ day_seven_python.py
@@ -149,6 +149,8 @@ while not end_of_game:
     # If lives goes down to 0 then the game should stop and it should print "You lose."
     if guess not in chosen_word:
         lives -= 1
+        if lives == 0:
+            end_of_game = True
     # Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
 
