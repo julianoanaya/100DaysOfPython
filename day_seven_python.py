6420b1378ab82d5cd8b23465a9265b57d8d00927
Julian Anaya <juliananaya531@gmail.com>
2024-02-06 19:24:03 -0800

    day 7 building hangman game part 1 of 2

--- day_seven_python.py
+++ day_seven_python.py
@@ -3,6 +3,10 @@
 word_list = ["aardvark", "baboon", "camel"]
 
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+import random
+
+word_chosen = random.choice(word_list)
+print(word_chosen)
 
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
