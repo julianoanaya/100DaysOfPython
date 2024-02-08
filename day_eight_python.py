b6d5df10962bc9d6d1a8a058ec8efcd7391cc9a4
Julian Anaya <juliananaya531@gmail.com>
2024-02-08 14:27:59 -0800

    day 8 exercise 2 prime numebrs

--- day_eight_python.py
+++ day_eight_python.py
@@ -57,3 +57,24 @@ test_h = int(input())  # Height of wall (m)
 test_w = int(input())  # Width of wall (m)
 coverage = 5
 paint_calc(height=test_h, width=test_w, cover=coverage)
+
+
+# Write your code below this line 👇
+
+
+def prime_checker(number):
+    is_prime = True
+    for i in range(2, 9):
+        if number % i == 0:
+            is_prime = False
+    if is_prime:
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number.")
+
+
+# Write your code above this line 👆
+
+# Do NOT change any of the code below👇
+n = int(input())  # Check this number
+prime_checker(number=n)
