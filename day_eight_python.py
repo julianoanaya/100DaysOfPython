89325f794559cb7fa21834bd31d5c2507be99430
Julian Anaya <juliananaya531@gmail.com>
2024-02-08 13:44:21 -0800

    day 8 part 1 exercise functions w/ key arguements input

--- day_eight_python.py
+++ day_eight_python.py
@@ -32,9 +32,28 @@ greet_with("Angela", "London")
 
 
 # function with keyword arguments
-def greet_with(name, location):
-    print(f"Hello {name}")
-    print(f"What is it like in {location}?")
+# def greet_with(name, location):
+#     print(f"Hello {name}")
+#     print(f"What is it like in {location}?")
 
 
 greet_with(location="London", name="Booby")
+
+# Write your code below this line 👇
+import math
+
+
+def paint_calc(height, width, cover):
+    number_of_cans = (height * width) / cover
+    rounded_cans = math.ceil(number_of_cans)
+    print(f"You'll need {rounded_cans} cans of paint.")
+
+
+# Write your code above this line 👆
+# Define a function called paint_calc() so the code below works.
+
+# 🚨 Don't change the code below 👇
+test_h = int(input())  # Height of wall (m)
+test_w = int(input())  # Width of wall (m)
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
