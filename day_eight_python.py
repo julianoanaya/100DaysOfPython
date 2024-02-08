d99eb5b3b90372fa7b880ad36306cdf7973cfbf2
Julian Anaya <juliananaya531@gmail.com>
2024-02-08 15:24:44 -0800

    day 8 final project cipher text part 2 made decode part

--- day_eight_python.py
+++ day_eight_python.py
@@ -107,7 +107,34 @@ alphabet = [
     "x",
     "y",
     "z",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
 ]
+
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
@@ -138,7 +165,29 @@ def encrypt(plain_text, num_shifts):
 ##HINT: How do you get the index of an item in a list:
 # https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
 
+
 ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+# TODO-4: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+def decrypt(cipher_text, shift_amount):
+    plain_text = ""
+    for letter in cipher_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        new_letter = alphabet[new_position]
+        plain_text += new_letter
+    print(plain_text)
+    # TODO-5: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.
+    # e.g.
+    # cipher_text = "mjqqt"
+    # shift = 5
+    # plain_text = "hello"
+    # print output: "The decoded text is hello"
+
+
+# TODO-6: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+if direction == "encode":
+    encrypt(text, shift)
+elif direction == "decode":
+    decrypt(text, shift)
 
 # TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
-encrypt(text, shift)
