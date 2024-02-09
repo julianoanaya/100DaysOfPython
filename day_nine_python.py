c228667c60bc96fd3e7979540a77b9f0b027b648
Julian Anaya <juliananaya531@gmail.com>
2024-02-09 14:10:45 -0800

    day 9 learning to add information from hash

--- day_nine_python.py
+++ day_nine_python.py
@@ -1,6 +1,13 @@
 programming_dictionary = {
     "Bug": "An error in a program that prevents the program from running as expected.",
     "Function": "A piece of code that you can easily call over and over again.",
-    "Loop": "The action of doing something over and over again.",
+    # "Loop": "The action of doing something over and over again.",
 }
 print(programming_dictionary["Bug"])
+
+# adding information
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+print(programming_dictionary)
+
+# create an empty dictionary
+empty_dictionary = {}
