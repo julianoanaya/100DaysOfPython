987657b617b1a34b4bf46fc8fda6085e912ef4cd
Julian Anaya <juliananaya531@gmail.com>
2024-02-09 15:02:03 -0800

    day 9 learning nesting a dictionary in a list

--- day_nine_python.py
+++ day_nine_python.py
@@ -74,3 +74,19 @@ travel_log = {
     },
 }
 print(travel_log)
+
+# Nesting a dictionary in a list
+travel_log = [
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12,
+        "plane_rides": 4,
+    },
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 5,
+        "plane_rides": 3,
+    },
+]
