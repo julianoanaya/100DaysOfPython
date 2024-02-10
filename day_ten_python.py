ef8a5dbd585c121da7c781e8f6d44ad1948e0e5e
Julian Anaya <juliananaya531@gmail.com>
2024-02-10 14:59:35 -0800

    day 10 learning retuen function

---
+++ day_ten_python.py
@@ -0,0 +1,11 @@
+# Function with outputs
+
+
+def format_name(f_name, l_name):
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    return f"{formatted_f_name} {formatted_l_name}"
+
+
+formatted_string = format_name("angela", "YU")
+print(formatted_string)
