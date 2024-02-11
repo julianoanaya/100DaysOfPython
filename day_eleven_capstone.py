fd3312141f5000a70c9b6f29f4c5eaa60ad98dc2
Julian Anaya <juliananaya531@gmail.com>
2024-02-11 14:50:29 -0800

    day 11 blackjack made ui a little better

--- day_eleven_capstone.py
+++ day_eleven_capstone.py
@@ -1,24 +1,26 @@
 import random
 
+play_game = input(
+    "Do you want to play a game of Blackjack? type 'y' for yes and 'n' for no "
+)
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
 user_cards = []
-# random_card = random.choice(cards)
-for stating_hand in range(1, 3):
-    random_card = random.choice(cards)
-    user_cards.append(random_card)
-print(user_cards)
+computer_hand = []
+
+if play_game == "y":
+    for stating_hand in range(1, 3):
+        random_card = random.choice(cards)
+        user_cards.append(random_card)
+    for computer_starting_hand in range(1, 3):
+        random_card = random.choice(cards)
+        computer_hand.append(random_card)
 
 first_cards = user_cards[0]
 second_card = user_cards[1]
 score = first_cards + second_card
 print(score)
 
+print(f"Your cards: {user_cards}, current score: {score}")
+print(f"Computer's first hand: {computer_hand[0]}")
 
 # for current_user_score in range(1, user_hand + 1):
-
-computer_hand = []
-for computer_starting_hand in range(1, 3):
-    random_card = random.choice(cards)
-    computer_hand.append(random_card)
-# print(computer_hand[0])
