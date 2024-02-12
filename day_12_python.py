abd374eecd50772a9390116b1d97031b68fc2e78
Julian Anaya <juliananaya531@gmail.com>
2024-02-12 15:18:12 -0800

    day 12 learning local scope

--- day_12_python.py
+++ day_12_python.py
@@ -10,3 +10,13 @@ def increase_enemies():
 
 increase_enemies()
 print(f"enemies outside function: {enemies}")
+
+# local scope
+
+
+def drink_potion():
+    potion_strength = 2
+    print(potion_strength)
+
+
+drink_potion()
