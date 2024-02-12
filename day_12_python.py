b2f1a7403135a6e66964fdac0dd1ba9929ffa5ed
Julian Anaya <juliananaya531@gmail.com>
2024-02-12 15:28:54 -0800

    day 12 no block scope

--- day_12_python.py
+++ day_12_python.py
@@ -31,3 +31,11 @@ def game()
   drink_potion
 
 print(player_health)
+
+
+#THERE is no block scope
+game_level=3
+enemies=["skeleton","zombie","alien"]
+if game_level<5:
+  new_enemy=enemies[0]
+print(new_enemy)
