920ef53c8ab0f6b455b8cc48d306ebeee259a4ea
Julian Anaya <juliananaya531@gmail.com>
2024-02-13 13:45:16 -0800

    day 13 debuging exercise 2, debbing - had to chage input type

--- day_13_python.py
+++ day_13_python.py
@@ -58,3 +58,18 @@ if number % 2 == 0:
     print("This is an even number.")
 else:
     print("This is an odd number.")
+
+
+# Which year do you want to check?
+year = int(input())
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
