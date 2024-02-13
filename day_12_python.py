4eee1557634bb43af40968d18c59d20a9eac9b71
Julian Anaya <juliananaya531@gmail.com>
2024-02-12 16:34:12 -0800

    day 12 setting up begining of guess a number game

--- day_12_python.py
+++ day_12_python.py
@@ -1,55 +1,68 @@
-################### Scope ####################
+# ################### Scope ####################
 
-enemies = 1
-
-
-def increase_enemies():
-  enemies = 2
-  print(f"enemies inside function: {enemies}")
+# enemies = 1
 
 
-increase_enemies()
-print(f"enemies outside function: {enemies}")
-
-# local scope
-
-
-# def drink_potion():
-#     potion_strength = 2
-#     print(potion_strength)
+# def increase_enemies():
+#   enemies = 2
+#   print(f"enemies inside function: {enemies}")
 
 
-# drink_potion()
+# increase_enemies()
+# print(f"enemies outside function: {enemies}")
 
-# global scope
-player_health = 10
-
-def game()
-  def drink_potion():
-      potion_strength = 2
-      print(player_health)
-  drink_potion
-
-print(player_health)
+# # local scope
 
 
-
-#THERE is no block scope
-game_level=3
-enemies=["skeleton","zombie","alien"]
-def create_enemy():
-  if game_level<5:
-    new_enemy=enemies[0]
-  print(new_enemy)
-  
-def increase_enemies():
-  print(f"enemies inside function: {enemies}")
-  return enemies+1
+# # def drink_potion():
+# #     potion_strength = 2
+# #     print(potion_strength)
 
 
-enemies = increase_enemies()
-print(f"enemies outside function: {enemies}")
+# # drink_potion()
 
-#global constants
-PI=3.14159
-URL = "https://www.google.com"
+# # global scope
+# player_health = 10
+
+# def game()
+#   def drink_potion():
+#       potion_strength = 2
+#       print(player_health)
+#   drink_potion
+
+# print(player_health)
+
+
+# #THERE is no block scope
+# game_level=3
+# enemies=["skeleton","zombie","alien"]
+# def create_enemy():
+#   if game_level<5:
+#     new_enemy=enemies[0]
+#   print(new_enemy)
+
+# def increase_enemies():
+#   print(f"enemies inside function: {enemies}")
+#   return enemies+1
+
+
+# enemies = increase_enemies()
+# print(f"enemies outside function: {enemies}")
+
+# #global constants
+# PI=3.14159
+# URL = "https://www.google.com"
+
+
+import random
+
+# from art import logo
+
+# print(logo)
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+difficulty = input("Choose a difficulty: Type 'easy or 'hard: ")
+
+def random_number():
+def guess_number():
+    print(5)
