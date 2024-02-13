422e0e30816ecfe311240d12bd53c80961918546
Julian Anaya <juliananaya531@gmail.com>
2024-02-13 13:05:33 -0800

    day 13 debuging code part 2 randint had a higher endpoint and caused it to bug only sometimes

--- day_13_python.py
+++ day_13_python.py
@@ -10,11 +10,12 @@ def my_function():
 
 my_function()
 
-# # Reproduce the Bug
-# from random import randint
-# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-# dice_num = randint(1, 6)
-# print(dice_imgs[dice_num])
+# Reproduce the Bug
+from random import randint
+
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(0, 5)
+print(dice_imgs[dice_num])
 
 # # Play Computer
 # year = int(input("What's your year of birth?"))
