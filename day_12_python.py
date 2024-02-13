d117f0142ab379ee2a07405c7d96fa01253b7c52
Julian Anaya <juliananaya531@gmail.com>
2024-02-12 16:41:14 -0800

    day 12 made random number function

--- day_12_python.py
+++ day_12_python.py
@@ -62,7 +62,16 @@ import random
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 difficulty = input("Choose a difficulty: Type 'easy or 'hard: ")
+answer_number = 0
+
 
 def random_number():
+    rand_num = random.randint(1, 100)
+    return rand_num
+
+
+print(random_number())
+
+
 def guess_number():
     print(5)
