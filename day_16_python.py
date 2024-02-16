085236e61a20916b1ab9cf26eb84ebf00a07f4de
Julian Anaya <juliananaya531@gmail.com>
2024-02-16 14:19:46 -0800

    day 16 coffee machine with oop

--- day_16_python.py
+++ day_16_python.py
@@ -1,28 +1,37 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
 
-timmy.color("blue")
+# timmy.color("blue")
 
-timmy.forward(100)
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# timmy.forward(100)
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-from prettytable import PrettyTable
+# from prettytable import PrettyTable
 
-table = PrettyTable()
-table.add_column(
-    "Pokemon Name",
-    ["Pikachu", "Squirtle", "Charmander"],
-)
-table.add_column(
-    "Type",
-    ["Electric", "Water", "Fire"],
-)
-table.align = "l"
+# table = PrettyTable()
+# table.add_column(
+#     "Pokemon Name",
+#     ["Pikachu", "Squirtle", "Charmander"],
+# )
+# table.add_column(
+#     "Type",
+#     ["Electric", "Water", "Fire"],
+# )
+# table.align = "l"
 
 
-print(table)
+# print(table)
+
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+
+menu = Menu()
