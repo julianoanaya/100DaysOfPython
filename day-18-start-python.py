dfe04364d8a5a1c6d5e90220867348b3eb1e0cde
Julian Anaya <juliananaya531@gmail.com>
2024-02-18 13:43:47 -0800

    day 18 - lectures version

--- day-18-start-python.py
+++ day-18-start-python.py
@@ -17,7 +17,7 @@ import random
 
 tim = t.Turtle()
 tim.shape("turtle")
-num_sides = 3
+# num_sides = 3
 screen = Screen()
 screen.colormode(255)
 r = random.randint(1, 255)
@@ -45,21 +45,28 @@ tim.color(r, g, b)
 #     for _ in range(num_sides):
 #         tim.forward(100)
 #         tim.right(angle)
-
-
+#
+#
 # for shape_side_n in range(3, 11):
 #     draw_shape(shape_side_n)
-
 tim.speed(800)
-tim.pensize(3)
+tim.pensize(5)
 forward_amount = [5, 10, 15]
 direction = [0, 90, 180, 270]
-for _ in range(1, 200):
+
+
+def random_color():
     r = random.randint(1, 255)
     g = random.randint(1, 255)
     b = random.randint(1, 255)
-    tim.color(r, g, b)
+    random_color = (r, g, b)
+    return random_color
+
+
+for _ in range(200):
+    tim.color(random_color())
     tim.forward(random.choice(forward_amount))
     tim.setheading(random.choice(direction))
+
 
 screen.exitonclick()
