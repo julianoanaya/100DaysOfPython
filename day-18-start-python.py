78eee19fffc9098f0f95a0df1ad979fb0655868f
Julian Anaya <juliananaya531@gmail.com>
2024-02-18 11:47:29 -0800

    day 18 - drawing a square

--- day-18-start-python.py
+++ day-18-start-python.py
@@ -6,6 +6,11 @@ timmy_the_turtle.color("violet")
 timmy_the_turtle.forward(100)
 timmy_the_turtle.right(90)
 
+for i in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
+    print("Hello")
+
 
 screen = Screen()
 screen.exitonclick()
