14b0e39e72b1a57cf99d1ea4b6d465f1668be438
Julian Anaya <juliananaya531@gmail.com>
2024-02-19 14:45:52 -0800

    day 19 - made starting race with 6 turtles

--- day-19/day-19-start.py
+++ day-19/day-19-start.py
@@ -1,13 +1,28 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
 screen = Screen()
+screen.setup(500, 400)
+user_bet = screen.textinput(
+    "Make your bet!", "Which turtle will win the race? Enter a color: "
+)
+colors = ["red", "blue", "green", "yellow", "orange", "purple"]
+name = ["tim", "tom", "tat", "tem", "tam", "tum"]
+y_position = [-100, -66, -33, 33, 66, 100]
+# i = len(colors)
+# print(i)
+# tim = Turtle(shape="turtle")
+# tim.color("red")
+# tim.penup()
+# tim.goto(x=-235, y=-100)
 
 
-def move_forwards():
-    tim.forward(10)
+def make_turtle_starting_race():
+    for i in range(0, 6):
+        name[i] = Turtle(shape="turtle")
+        name[i].color(colors[i])
+        name[i].penup()
+        name[i].goto(x=-235, y=y_position[i])
 
 
-screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+make_turtle_starting_race()
 screen.exitonclick()
