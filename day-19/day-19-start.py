5f016cbabc449233e4b91b4397da968a8d733998
Julian Anaya <juliananaya531@gmail.com>
2024-02-19 15:30:05 -0800

    day 19 - made the turtles move at a random forward interval

--- day-19/day-19-start.py
+++ day-19/day-19-start.py
@@ -1,28 +1,40 @@
+import turtle
 from turtle import Turtle, Screen
+import random
 
+is_race_on = False
 screen = Screen()
 screen.setup(500, 400)
 user_bet = screen.textinput(
     "Make your bet!", "Which turtle will win the race? Enter a color: "
 )
 colors = ["red", "blue", "green", "yellow", "orange", "purple"]
-name = ["tim", "tom", "tat", "tem", "tam", "tum"]
 y_position = [-100, -66, -33, 33, 66, 100]
-# i = len(colors)
-# print(i)
-# tim = Turtle(shape="turtle")
-# tim.color("red")
-# tim.penup()
-# tim.goto(x=-235, y=-100)
+all_turtle = []
 
 
-def make_turtle_starting_race():
-    for i in range(0, 6):
-        name[i] = Turtle(shape="turtle")
-        name[i].color(colors[i])
-        name[i].penup()
-        name[i].goto(x=-235, y=y_position[i])
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[turtle_index])
+    new_turtle.goto(x=-235, y=y_position[turtle_index])
+    all_turtle.append(new_turtle)
 
 
-make_turtle_starting_race()
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtle:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            wining_color = turtle.pencolor()
+            if wining_color == user_bet:
+                print(f"You have won! The {wining_color} turtle is the winner")
+            else:
+                print(f"You have lost! The {wining_color} turtle is the winner")
+
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
+
 screen.exitonclick()
