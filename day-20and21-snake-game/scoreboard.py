5ca6e6697aba7a1e910d1ee9bc5fd4c77e351567
Julian Anaya <juliananaya531@gmail.com>
2024-02-23 16:07:54 -0800

    day 21 implented scorebaord - made it so that it gains a point after it collects a food and clears the previos score

--- day-20and21-snake-game/scoreboard.py
+++ day-20and21-snake-game/scoreboard.py
@@ -9,9 +9,13 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(0, 270)
         self.score = 0
-        self.write(f"Score = {self.score}", False, "center", ("Arial", 24, "normal"))
         self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score = {self.score}", False, "center", ("Arial", 24, "normal"))
 
     def increase_score(self):
         self.score += 1
-        self.write(f"Score = {self.score}", False, "center", ("Arial", 24, "normal"))
+        self.clear()
+        self.update_scoreboard()
