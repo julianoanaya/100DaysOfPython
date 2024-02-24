3c157152c915f7fe15a0d279525b4d7d5ab56777
Julian Anaya <juliananaya531@gmail.com>
2024-02-23 16:03:22 -0800

    day 21 formatted score board

--- day-20and21-snake-game/scoreboard.py
+++ day-20and21-snake-game/scoreboard.py
@@ -6,6 +6,12 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.color("white")
+        self.penup()
+        self.goto(0, 270)
         self.score = 0
         self.write(f"Score = {self.score}", False, "center", ("Arial", 24, "normal"))
-        # self.refresh_score()
+        self.hideturtle()
+
+    def increase_score(self):
+        self.score += 1
+        self.write(f"Score = {self.score}", False, "center", ("Arial", 24, "normal"))
