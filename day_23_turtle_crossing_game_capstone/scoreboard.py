aef39d521bdaa04055acf81ac1343cc190e10439
Julian Anaya <juliananaya531@gmail.com>
2024-03-01 18:54:37 -0800

    day 23 made car manager class and made it so that it makes cars of diffreent colors and made it so the cars move

--- day_23_turtle_crossing_game_capstone/scoreboard.py
+++ day_23_turtle_crossing_game_capstone/scoreboard.py
@@ -1,16 +1,5 @@
-import time
-from turtle import Screen
-from player import Player
-from car_manager import CarManager
-from scoreboard import Scoreboard
+FONT = ("Courier", 24, "normal")
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.tracer(0)
-player = Player()
-game_is_on = True
-while game_is_on:
-    time.sleep(0.1)
-    screen.update()
 
-screen.exitonclick()
+class Scoreboard:
+    pass
