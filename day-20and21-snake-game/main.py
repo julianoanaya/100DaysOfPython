069277aa34855dff148b05dcc41015609d0f279a
Julian Anaya <juliananaya531@gmail.com>
2024-03-04 15:50:23 -0800

    day 24 revisting day 21 and 22 snake game made a highscore so that it keeps that info

--- day-20and21-snake-game/main.py
+++ day-20and21-snake-game/main.py
@@ -26,10 +26,13 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+    # detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         scoreboard.increase_score()
+
+    # detect collision with wall
     if (
         snake.head.xcor() > 280
         or snake.head.xcor() < -280
@@ -39,11 +42,12 @@ while game_is_on:
         game_is_on = False
         scoreboard.game_over()
 
-        # detect collision with tail...
+    # detect collision with tail...
     for segment in snake.segments:
         if segment == snake.head:
             pass
         elif snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
+
 screen.exitonclick()
--- day-20and21-snake-game/scoreboard.py
+++ day-20and21-snake-game/scoreboard.py
@@ -1,5 +1,8 @@
 from turtle import Turtle
 
+ALIGNMENT = "center"
+FONT = ("Arial", 24, "normal")
+
 
 class Scoreboard(Turtle):
 
@@ -9,15 +12,24 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(0, 270)
         self.score = 0
+        self.high_score = 0
         self.hideturtle()
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score = {self.score}", False, "center", ("Arial", 24, "normal"))
+        self.clear()
+        self.write(
+            f"Score = {self.score} High Score: {self.high_score}",
+            False,
+            ALIGNMENT,
+            FONT,
+        )
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", False, "center", ("Arial", 24, "normal"))
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
