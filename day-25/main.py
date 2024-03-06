e00dd223ad278388526d90f8aa3f98d2492401e1
Julian Anaya <juliananaya531@gmail.com>
2024-03-06 13:46:34 -0800

    day 25 made it so that it prints just the temerature as an interager without the word temp

--- day-25/main.py
+++ day-25/main.py
@@ -5,6 +5,13 @@ import csv
 
 with open("weather_data.csv") as data_files:
     data = csv.reader(data_files)
+    temperature = []
     print(data)
     for row in data:
-        print(row)
+        temperature.append(row[1])
+temperature.pop(0)
+real_temperature = []
+for num in temperature:
+    integer = int(num)
+    real_temperature.append(integer)
+print(real_temperature)
