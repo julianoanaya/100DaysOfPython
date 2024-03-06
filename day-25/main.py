5663e9a781566fec129ffe8e01977b9601b8cf5f
Julian Anaya <juliananaya531@gmail.com>
2024-03-06 13:20:07 -0800

    day 25 used the csv import to make it print cleaner

--- day-25/main.py
+++ day-25/main.py
@@ -1,3 +1,10 @@
-with open("weather_data.csv") as data:
-    weather = data.readlines()
-    print(weather)
+# with open("weather_data.csv") as data:
+#     weather = data.readlines()
+#     print(weather)
+import csv
+
+with open("weather_data.csv") as data_files:
+    data = csv.reader(data_files)
+    print(data)
+    for row in data:
+        print(row)
