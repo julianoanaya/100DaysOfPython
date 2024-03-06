7834c36cfae1eaa1c9244e3585ffbee723b41dd6
Julian Anaya <juliananaya531@gmail.com>
2024-03-06 14:33:31 -0800

    day 25 installed pandas and made it using pandas

--- day-25/main.py
+++ day-25/main.py
@@ -3,15 +3,20 @@
 #     print(weather)
 import csv
 
-with open("weather_data.csv") as data_files:
-    data = csv.reader(data_files)
-    temperature = []
-    print(data)
-    for row in data:
-        temperature.append(row[1])
-temperature.pop(0)
-real_temperature = []
-for num in temperature:
-    integer = int(num)
-    real_temperature.append(integer)
-print(real_temperature)
+# with open("weather_data.csv") as data_files:
+#     data = csv.reader(data_files)
+#     temperature = []
+#     print(data)
+#     for row in data:
+#         temperature.append(row[1])
+# temperature.pop(0)
+# real_temperature = []
+# for num in temperature:
+#     integer = int(num)
+#     real_temperature.append(integer)
+# print(real_temperature)
+
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print(data)
