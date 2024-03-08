9742eda06782be0270239fbfe46b4c0a1b41269a
Julian Anaya <juliananaya531@gmail.com>
2024-03-07 17:43:27 -0800

    day 26 audiorium lesson 30

--- day-26/list_comprehension.py
+++ day-26/list_comprehension.py
@@ -23,3 +23,23 @@ result = [
 
 # Write your code 👆 above:
 print(result)
+
+
+with open("file1.txt") as data_1:
+    content = data_1.readlines()
+
+stripped_number = [int(number.strip()) for number in content]
+# print(stripped_number)
+
+with open("file2.txt") as data_2:
+    data = data_2.readlines()
+
+number_file_2 = [int(number_2.strip()) for number_2 in data]
+# print(number_file_2)
+
+result = [list for list in stripped_number if list in number_file_2]
+# print(result)
+
+
+# Write your code above 👆
+print(result)
