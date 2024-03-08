5bf1f9387bb933f8c6bce38b9cf62a18d74c6bba
Julian Anaya <juliananaya531@gmail.com>
2024-03-08 15:42:04 -0800

    day 27 learned how to edit title with button and entry

--- day-27/learning_tkiner.py
+++ day-27/learning_tkiner.py
@@ -12,7 +12,20 @@ my_label.config(text="New Text")
 
 # button
 
-button = Button(text="Click Me")
+
+def button_click():
+    # my_label["text"] = "I got clicked"
+    new_text = input.get()
+    # my_label.config(text=input.get())
+    my_label.config(text=new_text)
+
+
+button = Button(text="Click Me", command=button_click)
 button.pack()
 
+# Entry
+input = Entry(width=10)
+input.pack()
+# input.get()
+print(input.get())
 window.mainloop()
