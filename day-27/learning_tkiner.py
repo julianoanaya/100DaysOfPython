32c281040d11c28d7a296da5e5165a4528043fbe
Julian Anaya <juliananaya531@gmail.com>
2024-03-08 15:26:15 -0800

    day 27 learned how to make button

--- day-27/learning_tkiner.py
+++ day-27/learning_tkiner.py
@@ -1,10 +1,18 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 # label
-my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
 my_label.pack()
+
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+# button
+
+button = Button(text="Click Me")
+button.pack()
 
 window.mainloop()
--- day-27/playground.py
+++ day-27/playground.py
@@ -1,26 +1,27 @@
-def add(*args):
-    sum = 0
-    for n in args:
-        sum += n
-    return sum
-
-
-print(add(32, 5, 4, 9, 10))
-
-
-def calculate(n, **kwargs):
-    print(kwargs)
-    # for key, value in kwargs.items():
-    #     print(key)
-    #     print(value)
-    #
-    # print(kwargs["add"])
-    n *= kwargs["add"]
-    n *= kwargs["multiply"]
-    print(n)
-
-
-calculate(2, add=3, multiply=5)
+# #
+# # def add(*args):
+# #     sum = 0
+# #     for n in args:
+# #         sum += n
+# #     return sum
+# #
+# #
+# # print(add(32, 5, 4, 9, 10))
+#
+#
+# def calculate(n, **kwargs):
+#     print(kwargs)
+#     # for key, value in kwargs.items():
+#     #     print(key)
+#     #     print(value)
+#     #
+#     # print(kwargs["add"])
+#     n *= kwargs["add"]
+#     n *= kwargs["multiply"]
+#     print(n)
+#
+#
+# calculate(2, add=3, multiply=5)
 
 
 class Car:
